51626409d383e0125a992ecb85e36c35b1750ae7
Joao Pedro <joaopedro10782@gmail.com>
2021-03-29 10:58:05 -0300

    deadlock semaphores

--- projeto_trems/trem.cpp
+++ projeto_trems/trem.cpp
@@ -1,7 +1,10 @@
 #include "trem.h"
 #include <QtCore>
 
-QMutex region1,region2,region3,region4,region5,region6,region7;
+QMutex region1,region2,region3,region4,region5,region6,region7;//basicos
+QSemaphore semaphore1(2),semaphore2(2),semaphore3(2);//deadlock triplo
+QSemaphore semaphore4(3),semaphore5(3);//deadlock quadruplo
+QSemaphore semaphore6(4);//deadlock quintuplo
 
 Trem::Trem(int ID, int x, int y){
     this->ID = ID;
@@ -26,9 +29,15 @@ void Trem::run(){
             case 1:
 
                 if(x==100 && y==10){
+                    semaphore6.acquire(1);
+                    semaphore4.acquire(1);
+                    semaphore1.acquire(1);
                     region1.lock();
                 }
                 if(x==100 && y==130){
+                    semaphore6.release(1);
+                    semaphore4.release(1);
+                    semaphore1.release(1);
                     region1.unlock();
                 }
                 if(x==130 && y==100){
@@ -51,21 +60,33 @@ void Trem::run(){
             case 2:
 
                 if(x==220 && y==10){
+                    semaphore6.acquire(1);
+                    semaphore5.acquire(1);
+                    semaphore3.acquire(1);
                     region2.lock();
                 }
                 if(x==220 && y==130){
+                    semaphore6.release(1);
+                    semaphore5.release(1);
+                    semaphore3.release(1);
                     region2.unlock();
                 }
                 if(x==250 && y==100){
+                    semaphore4.acquire(1);
+                    semaphore2.acquire(1);
                     region5.lock();
                 }
                 if(x==160 && y==130){
+                    semaphore4.release(1);
+                    semaphore2.release(1);
                     region5.unlock();
                 }
                 if(x==220 && y==130){
+                    semaphore1.acquire(1);
                     region4.lock();
                 }
                 if(x==130 && y==100){
+                    semaphore1.release(1);
                     region4.unlock();
                 }
                 if(x==160 && y==130){
@@ -88,9 +109,15 @@ void Trem::run(){
             case 3:
 
                 if(x==340 && y==130){
+                    semaphore6.acquire(1);
+                    semaphore5.acquire(1);
+                    semaphore3.acquire(1);
                     region6.lock();
                 }
                 if(x==250 && y==100){
+                    semaphore6.release(1);
+                    semaphore5.release(1);
+                    semaphore3.release(1);
                     region6.unlock();
                 }
                 if(x==280 && y==130){
@@ -112,15 +139,25 @@ void Trem::run(){
                 break;
             case 4:
                 if(x==70 && y==160){
+                    semaphore6.acquire(1);
+                    semaphore4.acquire(1);
+                    semaphore1.acquire(1);
                     region3.lock();
                 }
                 if(x==160 && y==130){
+                    semaphore6.release(1);
+                    semaphore4.release(1);
+                    semaphore1.release(1);
                     region3.unlock();
                 }
                 if(x==100 && y==130){
+                    semaphore5.acquire(1);
+                    semaphore2.acquire(1);
                     region4.lock();
                 }
                 if(x==190 && y==160){
+                    semaphore5.release(1);
+                    semaphore2.release(1);
                     region4.unlock();
                 }
                 if(x==160 && y==130){
@@ -143,15 +180,25 @@ void Trem::run(){
             case 5:
 
                 if(x==220 && y==250){
+                    semaphore6.acquire(1);
+                    semaphore4.acquire(1);
+                    semaphore5.acquire(1);
+                    semaphore2.acquire(1);
                     region7.lock();
                 }
                 if(x==220 && y==130){
+                    semaphore6.release(1);
+                    semaphore4.release(1);
+                    semaphore5.release(1);
+                    semaphore2.release(1);
                     region7.unlock();
                 }
                 if(x==190 && y==160){
+                    semaphore3.acquire(1);
                     region5.lock();
                 }
                 if(x==280 && y==130){
+                    semaphore3.release(1);
                     region5.unlock();
                 }
                 if(x==220 && y==130){
